2bb50fab42f88814a7aced9f15b6b0d351a3cefd
MR丶小白 <2718771714@qq.com>
2020-04-02 12:15:17 +0800

    跨域

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+                //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域的域名
+                .allowedOrigins("*")
+                //是否允许证书 不再默认开启
+                .allowCredentials(true)
+                //允许通过的方法 Get Post Delete Put
+                .allowedMethods("*")
+                //跨域最长时间
+                .maxAge(3600);
+    }
+}
