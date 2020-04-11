dd6b691c6b121b57e10f32e5789d1f101c885703
MR丶小白 <2718771714@qq.com>
2020-04-11 21:38:56 +0800

    ArticleController

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -5,10 +5,7 @@ import com.example.demo.result.Result;
 import com.example.demo.result.ResultUtil;
 import com.example.demo.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/article")
@@ -18,10 +15,18 @@ public class ArticleController {
     private ArticleService articleService;
 
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "/show/{id}")
     public Result getArticleById(@PathVariable("id") Integer id){
         Article article=articleService.findArticlebyId(id);
+        return ResultUtil.success(article);
+    }
 
+
+    @PostMapping(path = "/add")
+    public Result addArticle(@RequestBody Article article){
+        /*System.out.println(article);
+        System.out.println("123");*/
+        articleService.addArticle(article);
         return ResultUtil.success(null);
     }
 
